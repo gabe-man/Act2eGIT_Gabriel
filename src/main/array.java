7a9b9d54084f8cc10cdff117a372aa061bb56b1b
Gabriel Morales Martínez <gabrielmorales1996@gmail.com>
2021-11-28 20:11:52 +0100

    Creado metodo para meter valores en un array

---
+++ src/main/array.java
@@ -0,0 +1,12 @@
+package main;
+
+public class array {
+
+	public static void main(String[] args) {
+		int[] array=new int[20];
+		for (int i = 0; i < array.length; i++) {
+			array[i]=(int)((Math.random()*10)+1);
+		}
+	}
+
+}
